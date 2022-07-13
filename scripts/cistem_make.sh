dcaedca262343ccff9a51d89cab4f2f1298beb9c
Ben <ben@clearnoise.org>
2022-07-13 19:02:02 +0000

    Update CI to ensure the right wx and intel are used and add a test for gcc=20

--- scripts/cistem_make.sh
+++ scripts/cistem_make.sh
@@ -2,26 +2,40 @@
 
 build_dir=${1}
 
-if [ $2 ] ; then
+cpp_standard=${2}
+
+if [ $3 ] ; then
     n_threads=${2}
 else
     n_threads=$(($(nproc) / 2))
-fi  
+fi 
 
-# We need to first  build any modules
-cd ${build_dir}/src
+if [[ $cpp_standard == 17 ]]; then
+    cd ${build_dir}/
+    make -j${n_threads}
+else
+    if [[ $cpp_standard == 20 ]]; then
+        # We need to first  build any modules
+        cd ${build_dir}/src
 
-make -j${n_threads} libmodules.a
+        make -j${n_threads} libmodules.a
 
-# Now do the normal build
-cd ..
-make -j${n_threads} 
+        # Now do the normal build
+        cd ..
+        make -j${n_threads} 
 
-# Get the top level directory relative to the build dir
-top_srcdir=$(awk '/^top_srcdir/ {print $3}' Makefile)
+        # Get the top level directory relative to the build dir
+        top_srcdir=$(awk '/^top_srcdir/ {print $3}' Makefile)
 
-# Finally we need to fix the dependency files which are somewhat broken for modules as of now
-cd src
-${top_srcdir}/../scripts/module_deps_find.sh ${n_threads} ${top_srcdir}/../scripts/module_deps_fix.sh
+        # Finally we need to fix the dependency files which are somewhat broken for modules as of now
+        cd src
+        ${top_srcdir}/../scripts/module_deps_find.sh ${n_threads} ${top_srcdir}/../scripts/module_deps_fix.sh
+    else
+        echo "cpp_standard must be either 17 or 20"
+        exit 1
+    fi
+fi
+
+
 
 

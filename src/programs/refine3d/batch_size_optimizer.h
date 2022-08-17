835162695f5eaf6676e71e2e0026c8645b0e0b8a
himesb <himes.benjamin@gmail.com>
2022-08-17 13:13:07 +0000

    seems to be an accuracy regression

--- src/programs/refine3d/batch_size_optimizer.h
+++ src/programs/refine3d/batch_size_optimizer.h
@@ -16,29 +16,46 @@ class BatchSizeOptimizer {
     int start( ) {
         start_time = std::chrono::high_resolution_clock::now( );
         int returned_batch_size;
-        switch ( t ) {
-            case -2: {
-                current_batch[0]    = initial_start - initial_step;
-                returned_batch_size = current_batch[0];
-                break;
+        if ( t < -1 ) {
+            // We are in the warm-up phase, so we just return the batch size
+            returned_batch_size = initial_start;
+        }
+        else {
+            if ( t > max_search_iterations ) {
+                returned_batch_size = best_batch_size;
+
+                if ( print_best_batch_size ) {
+
+                    if ( omp_get_thread_num( ) == 0 ) {
+                        std::cerr << "\n\tOptimal batch size found: " << returned_batch_size << "\n";
+                        print_best_batch_size = false;
+                    }
+                }
             }
-            case -1: {
-                current_batch[1]    = initial_start + initial_step;
-                returned_batch_size = current_batch[1];
-                break;
-            }
-            default: {
-                returned_batch_size = current_batch[1];
-                break;
+            else {
+                switch ( t ) {
+                    case -1: {
+                        current_batch[0]    = initial_start - initial_step;
+                        returned_batch_size = current_batch[0];
+                        break;
+                    }
+                    case 0: {
+                        current_batch[1]    = initial_start + initial_step;
+                        returned_batch_size = current_batch[1];
+                        break;
+                    }
+                    default: {
+                        returned_batch_size = current_batch[1];
+                        break;
+                    }
+                }
             }
         }
-
-        // if ( omp_get_thread_num( ) == 0 ) {
-        //     std::cerr << "Current batch " << current_batch[1] << std::endl;
-        // }
-
         t++;
-        return returned_batch_size;
+        // if ( omp_get_thread_num( ) == 0 ) {
+        //     std::cerr << "Current batch " << returned_batch_size << std::endl;
+        // }
+        return 20; //returned_batch_size;
     };
 
     void lap( ) {
@@ -46,6 +63,7 @@ class BatchSizeOptimizer {
         // Reduce to milliseconds
         time_in_ms = ((ns_count / 100000) % 10 >= 5) ? 1 : 0;
         time_in_ms += (ns_count / 1000000);
+
 #ifdef PRINT_OPT_DEBUG
         if ( omp_get_thread_num( ) == 0 )
             std::cerr << "At time t: ms = " << t << " " << time_in_ms << std::endl;
@@ -53,46 +71,53 @@ class BatchSizeOptimizer {
     };
 
     void update_batch_size( ) {
-        // Get the measured time.
-        lap( );
-        if ( t == -1 ) {
-            current_time[0] = time_in_ms;
-        }
-        else {
 
-            current_time[1] = time_in_ms;
-
-            calc_gradient( );
-            if ( calculate_alpha ) {
-                // Because we don't know how large the steps will be, choose alpha to be ~half the intial step size
-                alpha           = float(initial_step) / 2.f; // fabsf(ceilf(10.f * float(initial_step)) / (gradient + epsilon));
-                calculate_alpha = false;
+        if ( t > -1 ) {
+            // Get the measured time.
+            lap( );
+            if ( t == 0 ) {
+                current_time[0] = time_in_ms;
             }
+            else {
 
-            t++;
-            beta1_of_t       = powf(beta1, t);
-            beta2_of_t       = powf(beta2, t);
-            m[1]             = beta1 * m[0] + (1 - beta1) * gradient;
-            v[1]             = beta2 * v[0] + (1 - beta2) * gradient * gradient;
-            m_hat            = m[1] / (1 - beta1_of_t);
-            v_hat            = v[1] / (1 - beta2_of_t);
-            current_step     = int(std::round(alpha * m_hat / (sqrtf(v_hat) + epsilon)));
-            current_batch[0] = current_batch[1];
-            // FIXME:: limits
-            current_batch[1] = std::min(100, std::max(1, current_batch[1] - current_step));
-            current_time[0]  = current_time[1];
+                current_time[1] = time_in_ms;
 
-            m[0] = m[1];
-            v[0] = v[1];
+                calc_gradient( );
+                if ( calculate_alpha ) {
+                    // Because we don't know how large the steps will be, choose alpha to be ~half the intial step size
+                    alpha           = float(initial_step) / 2.f; // fabsf(ceilf(10.f * float(initial_step)) / (gradient + epsilon));
+                    calculate_alpha = false;
+                }
+
+                beta1_of_t       = powf(beta1, t);
+                beta2_of_t       = powf(beta2, t);
+                m[1]             = beta1 * m[0] + (1 - beta1) * gradient;
+                v[1]             = beta2 * v[0] + (1 - beta2) * gradient * gradient;
+                m_hat            = m[1] / (1 - beta1_of_t);
+                v_hat            = v[1] / (1 - beta2_of_t);
+                current_step     = int(std::round(alpha * m_hat / (sqrtf(v_hat) + epsilon)));
+                current_batch[0] = current_batch[1];
+                // FIXME:: limits
+                current_batch[1] = std::min(100, std::max(min_batch_size, current_batch[1] - current_step));
+                current_time[0]  = current_time[1];
+
+                m[0] = m[1];
+                v[0] = v[1];
+
+                if ( time_in_ms < best_batch_time ) {
+                    best_batch_size = current_batch[1];
+                    best_batch_time = time_in_ms;
+                }
 #ifdef PRINT_OPT_DEBUG
-            if ( omp_get_thread_num( ) == 0 ) {
-                std::cerr << "Batch size: " << current_batch[1] << " step " << current_step << std::endl;
-                std::cerr << "Gradient and alpha : " << gradient << " " << alpha << std::endl;
-                std::cerr << "m_hat and v_hat : " << m_hat << " " << v_hat << std::endl;
-                std::cerr << "m[1] and v[1] : " << m[1] << " " << v[1] << "\n\n"
-                          << std::endl;
-            }
+                if ( omp_get_thread_num( ) == 0 ) {
+                    std::cerr << "Batch size: " << current_batch[1] << " step " << current_step << std::endl;
+                    std::cerr << "Gradient and alpha : " << gradient << " " << alpha << std::endl;
+                    std::cerr << "m_hat and v_hat : " << m_hat << " " << v_hat << std::endl;
+                    std::cerr << "m[1] and v[1] : " << m[1] << " " << v[1] << "\n\n"
+                              << std::endl;
+                }
 #endif
+            }
         }
         return;
     };
@@ -113,7 +138,7 @@ class BatchSizeOptimizer {
         alpha = 0.0;
         beta1_of_t;
         beta2_of_t;
-        t        = -2;
+        t        = -1;
         m[0]     = 0.0;
         m[1]     = 0.0;
         v[0]     = 0.0;
@@ -132,13 +157,17 @@ class BatchSizeOptimizer {
     }
 
   private:
+    const int warm_up_iterations    = 4;
+    const int max_search_iterations = warm_up_iterations + 20;
+    const int min_batch_size        = 5;
+
     float       alpha   = 0.0;
     const float beta1   = 0.9;
     const float beta2   = 0.999;
     const float epsilon = 1e-8;
     float       beta1_of_t;
     float       beta2_of_t;
-    int         t        = -2;
+    int         t        = -2 - warm_up_iterations;
     float       m[2]     = {0.f, 0.f};
     float       v[2]     = {0.f, 0.f};
     float       m_hat    = 0.0;
@@ -155,11 +184,13 @@ class BatchSizeOptimizer {
     const int initial_step  = 5;
     const int initial_start = 15;
 
-    int current_step    = initial_step;
-    int best_batch_size = initial_start;
+    int      current_step    = initial_step;
+    int      best_batch_size = initial_start;
+    uint64_t best_batch_time = std::numeric_limits<uint64_t>::max( );
 
     // We set the initial learning rate to produce a step size that is 1/2 the initial step assuming we have a decent idea where the optimum is
-    bool calculate_alpha = true;
+    bool calculate_alpha       = true;
+    bool print_best_batch_size = true;
 };
 
 #endif

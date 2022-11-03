0203fbe3b8d73674e0c801369a661b4e87fa3bbf
himesb <himes.benjamin@gmail.com>
2022-11-03 13:51:09 -0400

    add base scripts

---
+++ scripts/auto/Movie2Map.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# In the future, movie names will be fed in here as they appear on disk
+# For now, we'll just use what is in the directory.
+
+source params.sh
+
+mkdir -p $output_dir
+mkdir -p $output_dir/images
+mkdir -p $output_dir/volumes
+
+# ls $movie_dir/* | parallel --bar --progress -j${max_movies_per_gpu} ./auto_movie_align.sh {}
+
+./auto_sim_ref.sh
+
+
+
+
+
